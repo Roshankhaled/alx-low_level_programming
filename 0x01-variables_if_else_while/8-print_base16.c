e87e8c8660ce9dcee770e19a2f5507b7a8589dba
Roshankhaled <roshankhaled39@gmail.com>
2023-07-14 19:16:38 -0700

    c var

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+
+	putchar('\n');
+	return (0);
+}
